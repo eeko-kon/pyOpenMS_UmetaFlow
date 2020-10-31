5f2ea51d6454afe20f01f613103ea02ff896148e
eeko-kon <54527247+eeko-kon@users.noreply.github.com>
2020-10-31 18:11:39 +0100

    deleted some unnecessary files and fixed script in workflow AccurateMassSearchEngine()

--- WorkflowFirst.py
+++ WorkflowFirst.py
@@ -2,25 +2,20 @@ import numpy as np
 import pandas as pd
 import pyopenms
 from pyopenms import *
-
-options = PeakFileOptions()
-options.setMSLevels([1])
-fh = MzMLFile()
-fh.setOptions(options)
-
+import sys
 exp = MSExperiment()
 print("about load")
-MzMLFile().load("raw_files/20200723_NBC_UMETAB137_Albus_O_acu_POS_009.mzML", exp)
+MzMLFile().load("filename", exp)   #or MzMLFile().load(sys.argv[1].exp)
 print("successfully loaded")
 feature_map = FeatureMap()
 mass_traces = []
 mass_traces_split = []
 mass_traces_filtered = []
 
-peak_map = PeakMap()
-
 print('# Spectra:', len(exp.getSpectra()))
 print('# Chromatograms:', len(exp.getChromatograms()))
+
+peak_map = PeakMap()
 
 for chrom in exp.getChromatograms():
     peak_map.addChromatogram(chrom)
@@ -28,20 +23,15 @@ for chrom in exp.getChromatograms():
 for spec in exp.getSpectra():
     peak_map.addSpectrum(spec)
 
-mass_trace_detect = MassTraceDetection()
-mass_trace_detect.run(peak_map, mass_traces, 10000000000000)
-
+MassTraceDetection().run(peak_map, mass_traces, 10000000000000)
 print('# Mass traces:', len(mass_traces))
-
-elution_peak_detection = ElutionPeakDetection()
-elution_peak_detection.detectPeaks(mass_traces, mass_traces_split)
+ElutionPeakDetection().detectPeaks(mass_traces, mass_traces_split)
 print('# Mass traces split:', len(mass_traces_split))
 
 ff = FeatureFindingMetabo()
 ff.run(mass_traces_split,
     feature_map,
     mass_traces_filtered)
-
 
 print('# Mass traces filtered:', len(mass_traces_filtered))
 print('# Features:', feature_map.size())
@@ -54,19 +44,16 @@ fh.store('FeatureFindingMetabo.featureXML', feature_map)
 for p in feature_map:
     print(p.getRT(), p.getIntensity(), p.getMZ())
 
-
 deconv = MetaboliteFeatureDeconvolution()
 f_out= FeatureMap()
 cons_map0= ConsensusMap()
 cons_map1= ConsensusMap()
 deconcoluted= deconv.compute(feature_map, f_out, cons_map0, cons_map1)
 
-
 search= AccurateMassSearchEngine()
 parsefiles= search.init() 
 mztab= MzTab()
 hits= search._run_0(deconcoluted, mztab) 
-mztab.store("Masshits.mztab", hits)
-print(hits)
+
 
 
