57f38dd1cd83378af1ff853805e61c9b6308d10c
eeko-kon <54527247+eeko-kon@users.noreply.github.com>
2021-02-25 15:01:59 +0100

    sirius.store

--- Workflownew.py
+++ Workflownew.py
@@ -48,14 +48,27 @@ cons_map1= ConsensusMap()
 deconvoluted= deconv.compute(feature_map, f_out, cons_map0, cons_map1)
 deconvol = FeatureXMLFile()
 deconvol.store("devoncoluted.featureXML", feature_map)
-Sirius= SiriusMSFile()
-sirius= MSExperiment()
-feature_only= FeatureXMLFile() #?
-#bool no_mt_info??
-compoundinfo= SiriusMSFile()
-Sirius.store(exp, sirius, feature_map, feature_only, 10, #noideahere, compoundinfo)
 
-#Cython signature: void store(MSExperiment & spectra, String & msfile,
-#FeatureMapping_FeatureToMs2Indices & feature_ms2_spectra_map, bool & feature_only, 
-#int & isotope_pattern_iterations, bool no_mt_info, 
-#libcpp_vector[SiriusMSFile_CompoundInfo] v_cmpinfo)
+Sirius= SiriusMSFile()
+argument1= exp 
+argument2= SiriusTemporaryFileSystemObjects.getTmpMsFile()
+argument3= FeatureMapping_FeatureToMs2Indices()
+feature_only= True #SiriusAdapterAlgorithm.getFeatureOnly()==True
+#this is a parameter, which is called "feature_only" 
+#It is a boolean value (true/false) and if it is true you are using the  the feature information 
+#from in_featureinfo to reduce the search space to MS2 associated with a feature.
+#this is recommended when working with featureXML input, if you do NOT use it 
+#sirius will use every individual MS2 spectrum for estimation (and it will take ages)
+#bool feature_only = (sirius_algo.getFeatureOnly() == "true") ? true : false;
+
+Isotope_iter= 3 #SiriusAdapterAlgorithm.getIsotopePatternIterations()
+Isotopemasstraceinfo= False #This boolean value can lead to discarding the masstrace information from a feature and will usethe isotope_pattern_iterations instead -> so in your case it should be false - since you would like to use to feature information. 
+#SiriusAdapterAlgorithm.getNoMasstraceInfoIsotopePattern() == False
+CompoundInfo= [] #SiriusMSFile_CompoundInfo()
+Sirius.store(exp, argument2, argument3, feature_only, True, 3, False, CompoundInfo)
+
+#Cython signature: void store
+#(MSExperiment & spectra, String & msfile,
+# FeatureMapping_FeatureToMs2Indices & feature_ms2_spectra_map, bool & feature_only, 
+# int & isotope_pattern_iterations, bool no_mt_info, 
+# libcpp_vector[SiriusMSFile_CompoundInfo] v_cmpinfo)
