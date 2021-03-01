59872bebad21849b4a484e16b8b3d15ac9efd8c1
oliveralka <randalthor89@gmx.net>
2021-03-01 10:26:41 +0100

    FIX msfile

--- Workflownew.py
+++ Workflownew.py
@@ -7,7 +7,7 @@ exp = MSExperiment()
 
 import sys
 print("Loading")
-MzMLFile().load("GermicidinAstandard.mzML", exp)
+MzMLFile().load("./wf_testing/GermicidinAstandard.mzML", exp)
 print("Loaded")
 
 feature_map = FeatureMap()
@@ -36,36 +36,56 @@ print('# Features:', feature_map.size())
 feature_map.setUniqueIds()
 fh = FeatureXMLFile()
 print("Found", feature_map.size(), "features")
-fh.store('FeatureFindingMetabo.featureXML', feature_map)
+fh.store('./wf_testing/FeatureFindingMetabo.featureXML', feature_map)
 
 for p in feature_map:
     print(p.getRT(), p.getIntensity(), p.getMZ())
 
 deconv = MetaboliteFeatureDeconvolution()
-f_out= FeatureMap()
-cons_map0= ConsensusMap()
-cons_map1= ConsensusMap()
-deconvoluted= deconv.compute(feature_map, f_out, cons_map0, cons_map1)
+f_out = FeatureMap()
+cons_map0 = ConsensusMap()
+cons_map1 = ConsensusMap()
+deconvoluted = deconv.compute(feature_map, f_out, cons_map0, cons_map1)
 deconvol = FeatureXMLFile()
-deconvol.store("devoncoluted.featureXML", feature_map)
+deconvol.store("./wf_testing/devoncoluted.featureXML", feature_map)
 
-Sirius= SiriusMSFile()
-argument1= exp 
-argument2= SiriusTemporaryFileSystemObjects.getTmpMsFile()
-argument3= FeatureMapping_FeatureToMs2Indices()
-feature_only= True #SiriusAdapterAlgorithm.getFeatureOnly()==True
+# create temporary filesystem objects
+debug_level = 10
+sirius_tmp = SiriusTemporaryFileSystemObjects(debug_level)
+
+# TODO: Add preprocessing here! To use the featureMapping! 
+# https://github.com/OpenMS/OpenMS/blob/develop/src/utils/SiriusAdapter.cpp#L193
+
+# TODO: Check feature and/or spectra number
+# https://github.com/OpenMS/OpenMS/blob/develop/src/utils/SiriusAdapter.cpp#L201
+
+# construct sirius ms file object
+msfile = SiriusMSFile()
+
+# fill variables, which are used in the function
+argument1 = exp 
+# TODO: need to construct the feature mapping 
+feature_mapping = FeatureMapping_FeatureToMs2Indices() 
+feature_only = True #SiriusAdapterAlgorithm.getFeatureOnly()==True
+isotope_pattern_iterations = 3
+no_mt_info = False
+compound_info = [] #SiriusMSFile_CompoundInfo()
+
 #this is a parameter, which is called "feature_only" 
 #It is a boolean value (true/false) and if it is true you are using the  the feature information 
 #from in_featureinfo to reduce the search space to MS2 associated with a feature.
 #this is recommended when working with featureXML input, if you do NOT use it 
 #sirius will use every individual MS2 spectrum for estimation (and it will take ages)
 #bool feature_only = (sirius_algo.getFeatureOnly() == "true") ? true : false;
-
-Isotope_iter= 3 #SiriusAdapterAlgorithm.getIsotopePatternIterations()
-Isotopemasstraceinfo= False #This boolean value can lead to discarding the masstrace information from a feature and will usethe isotope_pattern_iterations instead -> so in your case it should be false - since you would like to use to feature information. 
 #SiriusAdapterAlgorithm.getNoMasstraceInfoIsotopePattern() == False
-CompoundInfo= [] #SiriusMSFile_CompoundInfo()
-Sirius.store(exp, argument2, argument3, feature_only, True, 3, False, CompoundInfo)
+
+msfile.store(exp, 
+             String(sirius_tmp.getTmpDir()), # has to be converted to an "OpenMS::String"
+             feature_mapping, 
+             feature_only,
+             isotope_pattern_iterations, 
+             no_mt_info, 
+             compound_info)
 
 #Cython signature: void store
 #(MSExperiment & spectra, String & msfile,
