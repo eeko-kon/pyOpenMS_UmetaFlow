4a22cf2dfaea2afd21c19f610bf22814b8d98a48
eeko-kon <eeko@biosustain.dtu.dk>
2021-11-11 20:49:11 +0100

    FFMident corrections

---
+++ FFMI.py
@@ -0,0 +1,167 @@
+from pyopenms import *
+import pandas as pd
+import numpy as np
+import os
+import glob
+import csv
+
+input_mzml_files = glob.glob('results/interim/*.mzML')
+
+raw_data=[]
+for filename in input_mzml_files:
+    exp= MSExperiment()
+    MzMLFile().load(filename, exp)
+    exp.sortSpectra(True)
+    raw_data.append(exp)
+
+ref_index = [i[0] for i in sorted(enumerate([exp.size() for exp in raw_data]), key=lambda x:x[1])][-1]
+aligner = MapAlignmentAlgorithmPoseClustering()
+aligner_par= aligner.getDefaults()
+aligner_par.setValue("max_num_peaks_considered", 3000)
+aligner.setParameters(aligner_par)
+aligner.setReference(raw_data[ref_index])
+
+for exp in raw_data[:ref_index] + raw_data[ref_index+1:]:
+    trafo = TransformationDescription()
+    aligner.align(exp, trafo)
+    transformer = MapAlignmentTransformer()
+    transformer.transformRetentionTimes(exp, trafo, True)
+
+for exp in raw_data:    
+    mzml_file = os.path.join("results", "", "FFMI", "", "interim", "", 'MapAligned_' + os.path.basename(exp.getLoadedFilePath())[19:-5] +".mzML")
+    MzMLFile().store(mzml_file, exp)
+
+# read tsv file and create list of FeatureFinderMetaboIdentCompound
+def metaboTableFromFile(path_to_library_file):
+    metaboTable = []
+    with open(path_to_library_file, 'r') as tsv_file:
+        tsv_reader = csv.reader(tsv_file, delimiter="\t")
+        next(tsv_reader) # skip header
+        for row in tsv_reader:
+            metaboTable.append(FeatureFinderMetaboIdentCompound(
+                row[0], # name
+                row[1], # sum formula
+                float(row[2]), # mass
+                [int(charge) for charge in row[3].split(',')], # charges
+                [float(rt) for rt in row[4].split(',')], # RTs
+                [float(rt_range) for rt_range in row[5].split(',')], # RT ranges
+                [float(iso_distrib) for iso_distrib in row[6].split(',')] # isotope distributions
+            ))
+    return metaboTable
+
+
+input_mzml_files=glob.glob("results/FFMI/interim/*.mzML")
+# load ms data from mzML file into MSExperiment
+for mzml_file in input_mzml_files:
+    spectra = MSExperiment()
+    MzMLFile().load(mzml_file, spectra)
+
+    # create FeatureFinderAlgorithmMetaboIdent and assign ms data
+    ff = FeatureFinderAlgorithmMetaboIdent()
+    ff.setMSData(spectra)
+
+    # read library generate a metabo table with compounds
+    metabo_table = metaboTableFromFile('epemicins.tsv')
+    params = ff.getParameters()
+    #params[b'debug']= 3
+    params[b'extract:mz_window'] = 5.0 
+    params[b'model:type']= b'none'
+    #params[b'extract:rt_window'] = 50.0
+    params[b'detect:peak_width'] = 20.0  #adjust for wide peaks
+    ff.setParameters(params)
+    # FeatureMap to store results
+    fm = FeatureMap()
+    # run the FeatureFinderMetaboIdent with the metabo_table and store results in fm
+    ff.run(metabo_table, fm)
+
+    # save FeatureMap to file
+    ff_file = os.path.join("results", "", "FFMI", "", "interim", "", 'FFMI_' + os.path.basename(mzml_file)[11:-5] +".featureXML")
+    FeatureXMLFile().store(ff_file, fm)
+
+from collections import defaultdict
+from functools import reduce
+from pathlib import Path
+from time import perf_counter
+import sys
+
+from IPython.core.display import display
+from pandas import CategoricalDtype
+import numpy as np
+from pyopenms import *
+import pandas as pd
+import os
+
+common_meta_value_types = {
+    b'label': 'U30',
+    b'spectrum_index': 'i',
+    b'score_fit': 'f',
+    b'score_correlation': 'f',
+    b'FWHM': 'f',
+    b'spectrum_native_id': 'U30',
+    b"num_of_masstraces" : "f"
+}
+
+class FeatureMapDF(FeatureMap):
+    def __init__(self):
+        super().__init__()
+    
+    # meta_values = None (default), 'all' or list of meta value names
+    def get_df(self, meta_values = None):
+        # get all possible meta value keys in a set
+        if meta_values == 'all':
+            meta_values = set()
+            for f in self:
+                mvs = []
+                f.getKeys(mvs)
+                for m in mvs:
+                    meta_values.add(m)
+        elif not meta_values: # if None, set to empty list
+            meta_values = []
+        
+        def gen(fmap: FeatureMap, fun):
+            for f in fmap:
+                yield from fun(f, meta_values)
+
+        def extract_meta_data(f: Feature, meta_values):
+            pep = f.getPeptideIdentifications()  # type: list[PeptideIdentification]
+            bb = f.getConvexHull().getBoundingBox2D()
+                
+            vals = [f.getMetaValue(m) if f.metaValueExists(m) else np.NA for m in meta_values]   # find some NA or None value for numpy
+            
+            if len(pep) != 0:
+                hits = pep[0].getHits()
+
+                if len(hits) != 0:
+                    besthit = hits[0]  # type: PeptideHit
+                    yield f.getUniqueId(), besthit.getSequence().toString(), f.getCharge(), f.getRT(), f.getMZ(), bb[0][0], bb[1][0], f.getMetaValue("PeptideRef"), f.getOverallQuality(), f.getIntensity()
+                else:
+                    yield f.getUniqueId(), None, f.getCharge(), f.getRT(), f.getMZ(), bb[0][0], bb[1][0], f.getMetaValue("PeptideRef"), f.getOverallQuality(), f.getIntensity()
+            else:
+                yield f.getUniqueId(), None, f.getCharge(), f.getRT(), f.getMZ(), bb[0][0], bb[1][0], f.getMetaValue("PeptideRef"), f.getOverallQuality(), f.getIntensity()
+
+        cnt = self.size()
+
+        mddtypes = [('id', np.dtype('uint64')), ('sequence', 'U200'), ('charge', 'i4'), ('RT', 'f'), ('mz', 'f'),
+                    ('RTstart', 'f'), ('RTend', 'f'), ("PeptideRef", 'U200'),
+                    ('quality', 'f'), ('intensity', 'f')]
+        
+        for meta_value in meta_values:
+            if meta_value in common_meta_value_types:
+                mddtypes.append((meta_value.decode(), common_meta_value_types[meta_value]))
+            else:
+                mddtypes.append((meta_value.decode(), 'U50'))
+        mdarr = np.fromiter(iter=gen(self, extract_meta_data), dtype=mddtypes, count=cnt)
+        df= pd.DataFrame(mdarr).set_index('id').sort_values("mz").drop(columns= "sequence")
+        return df
+
+
+input_feature_files = glob.glob('results/FFMI/interim/*.featureXML')
+
+for filename in input_feature_files:
+    fmap = FeatureMapDF()
+    FeatureXMLFile().load(filename, fmap)
+    DF= fmap.get_df()
+    feature_csv= os.path.join("results", "", "FFMI", "", 'features_' + os.path.basename(filename)[:-10] +"csv")
+    DF.to_csv(feature_csv)
+    print(os.path.basename(filename))
+    display(DF)
